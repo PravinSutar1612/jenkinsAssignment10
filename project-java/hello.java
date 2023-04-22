bd301bda76b09d34607aaa6120d920f58360f316
Pravin Sutar <128134416+PravinSutar1612@users.noreply.github.com>
2023-04-22 23:46:37 +0530

    Update hello.java

---
+++ project-java/hello.java
@@ -0,0 +1,10 @@
+package practice;
+public class hello
+ {
+     public static void main(String[] args) 
+	{
+		System.out.println("Hello Pravin");
+                                System.out.println("Poll Scm is used And Git is Connected");
+	}
+
+}
